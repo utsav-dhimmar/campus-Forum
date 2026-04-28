4d0772693c254315304977a7dbdad889fcd23372
utsav-dhimmar <utsavdhimmar987@gmail.com>
2026-04-28 20:29:34 +0530

    migrate to pnpm with monorepo and dockerize eniter app

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -7,9 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000/api",
+        target: process.env.VITE_API_TARGET || "http://localhost:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
